5930c79445a96e5cc48aa52637a07003a3c60dbf
ak7 <abdelrhman.khater111@gmail.com>
2019-04-03 00:01:28 +0200

    delete past files for proj1

--- proj 1/grab.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import socket
-
-
-s = socket.socket()
-s.connect(("attackdirect.samsclass.info", 22))
-print(s.recv(1024))
-s.close()
-
--- proj 1/grab1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import socket
-
-socket.setdefaulttimeout(1)
-s = socket.socket()
-s.connect(("attackdirect.samsclass.info", 80))
-print(s.recv(1024))
-s.close()
-
